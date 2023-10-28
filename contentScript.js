07251fd87f4b87a5acc0b16f3eb227798da5e11e
Valerie Tan <valerietanhx@gmail.com>
2023-10-28 11:45:47 -0400

    skeleton

--- contentScript.js
+++ contentScript.js
@@ -1,55 +1,64 @@
 (() => {
-    let youtubeLeftControls, youtubePlayer;
-    let currentVideo = "";
-    let currentVideoBookmarks = [];
+  let youtubeLeftControls, youtubePlayer;
+  let currentChat = "";
+  let currentVideoBookmarks = [];
 
-    chrome.runtime.onMessage.addListener((obj, sender, response) => {
-        const { type, value, videoId } = obj;
+  chrome.runtime.onMessage.addListener((obj, sender, response) => {
+    const { type, chatId } = obj;
 
-        if (type === "NEW") {
-            currentVideo = videoId;
-            newVideoLoaded();
-        }
-    });
+    console.log(chatId);
 
-    const newVideoLoaded = () => {
-        const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
-        console.log(bookmarkBtnExists);
-
-        if (!bookmarkBtnExists) {
-            const bookmarkBtn = document.createElement("img");
-
-            bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
-            bookmarkBtn.className = "ytp-button " + "bookmark-btn";
-            bookmarkBtn.title = "Click to bookmark current timestamp";
-
-            youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
-            youtubePlayer = document.getElementsByClassName("video-stream")[0];
-            
-            youtubeLeftControls.append(bookmarkBtn);
-            bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
-        }
+    if (type === "NEW") {
+      currentChat = chatId;
+      newChatLoaded();
     }
+  });
 
-    const addNewBookmarkEventHandler = () => {
-        const currentTime = youtubePlayer.currentTime;
-        const newBookmark = {
-            time: currentTime,
-            desc: "Bookmark at " + getTime(currentTime),
-        };
-        console.log(newBookmark);
+  const newChatLoaded = () => {
+    const chatGptReplies = document.getElementsByClassName("agent-turn");
+    console.log(chatGptReplies);
+    console.log("test", chatGptReplies[0]);
+    const lastReply = chatGptReplies[chatGptReplies.length - 1];
+    console.log("lastReply", lastReply);
+    const chatGptResponse = lastReply.innerText;
+    console.log(chatGptResponse);
+    //     const bookmarkBtnExists = document.getElementsByClassName("bookmark-btn")[0];
+    //     console.log(bookmarkBtnExists);
 
-        chrome.storage.sync.set({
-            [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
-        });
-    }
+    //     if (!bookmarkBtnExists) {
+    //         const bookmarkBtn = document.createElement("img");
 
-    newVideoLoaded();
+    //         bookmarkBtn.src = chrome.runtime.getURL("assets/bookmark.png");
+    //         bookmarkBtn.className = "ytp-button " + "bookmark-btn";
+    //         bookmarkBtn.title = "Click to bookmark current timestamp";
+
+    //         youtubeLeftControls = document.getElementsByClassName("ytp-left-controls")[0];
+    //         youtubePlayer = document.getElementsByClassName("video-stream")[0];
+
+    //         youtubeLeftControls.append(bookmarkBtn);
+    //         bookmarkBtn.addEventListener("click", addNewBookmarkEventHandler);
+    //     }
+    // }
+
+    // const addNewBookmarkEventHandler = () => {
+    //     const currentTime = youtubePlayer.currentTime;
+    //     const newBookmark = {
+    //         time: currentTime,
+    //         desc: "Bookmark at " + getTime(currentTime),
+    //     };
+    //     console.log(newBookmark);
+
+    //     chrome.storage.sync.set({
+    //         [currentVideo]: JSON.stringify([...currentVideoBookmarks, newBookmark].sort((a, b) => a.time - b.time))
+    //     });
+  };
+
+  newChatLoaded();
 })();
 
-const getTime = t => {
-    var date = new Date(0);
-    date.setSeconds(1);
+// const getTime = t => {
+//     var date = new Date(0);
+//     date.setSeconds(1);
 
-    return date.toISOString().substr(11, 0);
-}
+//     return date.toISOString().substr(11, 0);
+// }
